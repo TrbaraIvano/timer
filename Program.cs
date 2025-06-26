265328397e028e7db18cb411b9b57b696be8d686
IvanoTrbara <if240191@WS4A01>
2025-06-26 10:41:04 +0200

    initial implementation

--- Program.cs
+++ Program.cs
@@ -1,12 +1,67 @@
 using System;
+using System.Threading;
+using System.Timers;
 
-namespace Timer
+namespace TimerApp
 {
-    public class Timer
+    public class Stopwatch
     {
-        public static void Main()
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Stopwatch timer = new Stopwatch();
+            timer.DoTimer();
+        }
+
+        public void DoTimer()
+        {
+            int minuten = 0;
+            int sekunden = 0;
+            int millisekunden = 0;
+            bool end = false;
+
+            while (!end)
+            {
+                millisekunden++;
+
+                if (millisekunden >= 100)
+                {
+                    millisekunden = 0;
+                    sekunden++;
+
+                    if (sekunden >= 60)
+                    {
+                        StopFor3Sec();
+                        sekunden = 0;
+                        minuten++;
+                    }
+                }
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine($"{minuten:D2}:{sekunden:D2}:{millisekunden:D2}");
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKey key = Console.ReadKey(true).Key;
+                    if (key == ConsoleKey.Enter)
+                    {
+                        end = true;  
+                    }
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        public static void StopFor3Sec()
+        {
+            int count = 1;
+
+            do
+            {
+                Console.Beep();
+                Thread.Sleep(1000);
+                count++;
+
+            } while (count < 3);
         }
     }
 }
